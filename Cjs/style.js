5857b229de536264367d9c8bb9ff97e8684781cd
herineeda <higk@likelion.org>
2019-07-31 11:20:28 +0900

    webpage

---
+++ Cjs/style.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('#myCarousel').carousel('cycle');
+	$('#myCarousel2').carousel('cycle');
+	$(“#myCarousel”).carousel({ interval:0});
+	$(“#myCarousel2”).carousel({ interval:0});
+});
+
